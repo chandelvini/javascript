8b6243fda801411d5ea2a08df6fd5994bad862e4
chandelvini <cvinita914@gmail.com>
2024-12-25 11:19:30 +0000

    Datatype

---
+++ .devcontainer/o1_basics/Datatype.js
@@ -0,0 +1,24 @@
+//datatypes
+
+/* 1) number => range 2to power 53
+   2) bigint
+   3) string => ""
+   4) boolean => true/false
+   5) null => standalone value
+   6) undefined =>
+   7) symbol => unique
+   8) object  
+
+   typeof is used to check type of variable
+
+
+
+*/
+let rollno = 12
+let sub = null;
+let name;
+console.log(typeof rollno); //number
+console.log(typeof sub); // object
+console.log(typeof undefined); //undefined
+
+
