621d4046e4da0b467f65711eb69404a86eb5933f
chandelvini <cvinita914@gmail.com>
2024-12-25 11:51:40 +0000

    conversionof datatype

---
+++ .devcontainer/o1_basics/conversionoperation.js
@@ -0,0 +1,18 @@
+let marks = "33"
+let score = "33abc"
+score = null
+
+//if we want to convert the any type into number then what happen
+console.log(typeof marks)
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+
+//if we want to convert the any type to boolean 
+let num = 1;
+let str = " "
+let value = Boolean(str);
+console.log(value);
+
+//"" => give false
+// "vinita" => give true
+
