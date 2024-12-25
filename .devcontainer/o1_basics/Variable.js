238414856e441a1cd51222cdc5940bb3eb145b93
chandelvini <cvinita914@gmail.com>
2024-12-25 10:49:44 +0000

    variable done

---
+++ .devcontainer/o1_basics/Variable.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountEmail = "vinita@gmail.com"
+let accountPassword = "123"
+let accountCity = "jaipur"
+let accountState;
+//default value of variable is "undefined"
+
+console.log(accountCity);
+
+// if we want to print all in one way
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
