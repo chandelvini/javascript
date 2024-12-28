fd81dcaeb41308cbf2b3a121e90da7bb2c593b96
chandelvini <cvinita914@gmail.com>
2024-12-28 12:45:12 +0000

    operations

--- .devcontainer/o1_basics/conversionoperation.js
+++ .devcontainer/o1_basics/conversionoperation.js
@@ -16,3 +16,30 @@ console.log(value);
 //"" => give false
 // "vinita" => give true
 
+/****************************************************Operations************************************/
+
+let str1 = "hello"
+let str2 = " vinita"
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1+ "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+//console.log(true+);//invalid
+console.log(+ " ");
+
+let num1 ,num2,num3
+num1 = num2 = num3 = 2+2;
+
+let gamecounter = 100;
+gamecounter++;
+
+console.log(gamecounter);
+
+
+
+
