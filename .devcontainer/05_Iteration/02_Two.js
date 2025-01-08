a108c6c8e490d0a1513e9d6bcf1ae4fe836dfa2b
chandelvini <cvinita914@gmail.com>
2025-01-08 14:59:48 +0000

    while loop

---
+++ .devcontainer/05_Iteration/02_Two.js
@@ -0,0 +1,20 @@
+let index = 0;
+while(index <=10)
+{
+    console.log(`value of index is ${index}`)
+    index = index + 2
+}
+let MyArray = ['flash', 'batman','superman']
+
+let arr = 0;
+while(arr < MyArray.length)
+{
+    console.log(`value is ${MyArray}`)
+   arr =  arr + 1;
+}
+
+let score =11 ;
+do{
+    console.log(`socre is ${score}`)
+    score++;
+} while(score <=10);
